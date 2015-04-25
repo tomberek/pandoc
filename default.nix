647b49fcdaae977ff23363e78294e131590208e5
Ozan Sener <ozan@ozansener.com>
2015-04-25 12:28:28 +0700

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,21 @@
+{ mkDerivation, aeson, array, base, base64-bytestring, binary, blaze-html
+, blaze-markup, bytestring, containers, data-default, deepseq-generics
+, extensible-exceptions, happy, mtl, network-uri, old-locale, old-time
+, pandoc-types, parsec, random, scientific, SHA, syb, tagsoup, text, time
+, unordered-containers, vector, xml, yaml
+}:
+
+mkDerivation {
+  pname = "my-pandoc";
+  version = "1.13.2";
+  src = builtins.filterSource (path: type: baseNameOf path != ".git") ./.;
+  buildDepends = [
+    aeson array base base64-bytestring binary blaze-html blaze-markup
+    bytestring containers data-default deepseq-generics extensible-exceptions
+    happy mtl network-uri old-locale old-time pandoc-types parsec random
+    scientific SHA syb tagsoup text time unordered-containers vector xml yaml
+  ];
+  homepage = "http://johnmacfarlane.net/pandoc";
+  description = "Conversion between markup formats";
+  license = "GPL";
+}
